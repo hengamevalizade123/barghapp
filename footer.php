2478d58de80102f3ced57cfc31c84d0db1e7a73a
Hengameh Valizade <hengamehvalizade@gmail.com>
2025-10-08 12:24:01 +0330

    added:footer section

--- footer.php
+++ footer.php
@@ -11,23 +11,177 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'starter-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'starter-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'starter-theme' ), 'starter-theme', '<a href="https://markitasdone.com">Markitasdone</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer id="colophon" class="o-section c-section--footer site-footer p-0 relative">
+	<div class="o-section__wrapper">
+		<div class="c-footer">
+			<div class="c-footer__top u-flex gap-md items-center space-between">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/barghapp-logo.svg"
+					 alt="برقآپ">
+				<div class="c-footer__support u-flex gap-lg items-center">
+					<p class="m-0 u-flex gap-sm items-center">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M12 13.4299C13.7231 13.4299 15.12 12.0331 15.12 10.3099C15.12 8.58681 13.7231 7.18994 12 7.18994C10.2769 7.18994 8.88 8.58681 8.88 10.3099C8.88 12.0331 10.2769 13.4299 12 13.4299Z"
+								stroke="#545454" stroke-width="1.5"/>
+							<path
+								d="M3.62001 8.49C5.59001 -0.169998 18.42 -0.159997 20.38 8.5C21.53 13.58 18.37 17.88 15.6 20.54C13.59 22.48 10.41 22.48 8.39001 20.54C5.63001 17.88 2.47001 13.57 3.62001 8.49Z"
+								stroke="#545454" stroke-width="1.5"/>
+						</svg>
 
+						<span>آدرس : </span>
+						<span>
+						تهران، باغ فیض، خیابان عدل، پلاک ۴
+					</span>
+					</p>
+					<p class="m-0 u-flex gap-sm items-center">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M2.03 18.33C2.03 18.69 2.11 19.06 2.28 19.42C2.45 19.78 2.67 20.12 2.96 20.44C3.45 20.98 3.99 21.37 4.6 21.62C5.2 21.87 5.85 22 6.55 22C7.57 22 8.66 21.76 9.81 21.27C10.96 20.78 12.11 20.12 13.25 19.29C14.4 18.45 15.49 17.52 16.53 16.49C17.56 15.45 18.49 14.36 19.32 13.22C20.14 12.08 20.8 10.94 21.28 9.81C21.76 8.67 22 7.58 22 6.54C22 5.86 21.88 5.21 21.64 4.61C21.4 4 21.02 3.44 20.49 2.94C19.85 2.31 19.15 2 18.41 2C18.13 2 17.85 2.06 17.6 2.18C17.34 2.3 17.11 2.48 16.93 2.74L14.61 6.01C14.43 6.26 14.3 6.49 14.21 6.71C14.12 6.92 14.07 7.13 14.07 7.32C14.07 7.56 14.14 7.8 14.28 8.03C14.41 8.26 14.6 8.5 14.84 8.74L15.6 9.53C15.71 9.64 15.76 9.77 15.76 9.93C15.76 10.01 15.75 10.08 15.73 10.16C15.7 10.24 15.67 10.3 15.65 10.36C15.47 10.69 15.16 11.12 14.72 11.64C14.27 12.16 13.79 12.69 13.27 13.22C12.73 13.75 12.21 14.24 11.68 14.69C11.16 15.13 10.73 15.43 10.39 15.61C10.34 15.63 10.28 15.66 10.21 15.69C10.13 15.72 10.05 15.73 9.96 15.73C9.79 15.73 9.66 15.67 9.55 15.56L8.79 14.81C8.54 14.56 8.3 14.37 8.07 14.25C7.84 14.11 7.61 14.04 7.36 14.04C7.17 14.04 6.97 14.08 6.75 14.17C6.53 14.26 6.3 14.39 6.05 14.56L2.74 16.91C2.48 17.09 2.3 17.3 2.19 17.55C2.09 17.8 2.03 18.05 2.03 18.33Z"
+								stroke="#545454" stroke-width="1.5" stroke-miterlimit="10"/>
+							<path d="M5.5 9C5.5 8.4 5.97 7.48 6.67 6.73C7.31 6.04 8.16 5.5 9 5.5" stroke="#545454"
+								  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M2 9C2 5.13 5.13 2 9 2" stroke="#545454" stroke-width="1.5" stroke-linecap="round"
+								  stroke-linejoin="round"/>
+						</svg>
+						<span>پشتیبانی :  </span>
+						<a href="#" title="تلفن پشتیبانی" class="ltr">
+							۰۲۱ - ۳۳۵۸۹۲۴۸ | ۳۳۵۸۹۲۴۸
+						</a>
+					</p>
+				</div>
+
+			</div>
+			<div class="c-footer__bottom u-flex gap-lg items-start">
+				<div class="c-footer__col">
+					<div class="c-footer__title">
+						بــرقـــــآپ
+					</div>
+					<p class="txt-justify">
+						لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+						چاپگرها و فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهایتکنولوژی مورد نیاز، و
+						کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، و آینده، شناخت فراواسا مورد استفاده قرار
+						گیرد. آینده، شناخت فراواسا مورد استفاده طراحان گرافیک .
+					</p>
+				</div>
+				<div class="c-footer__col">
+					<div class="c-footer__title">
+						دسترسی سریع
+					</div>
+					<ul class="u-flex gap-sm u-flex--column">
+						<li>
+							<a href="/about-us/" title="درباره ما">درباره ما</a>
+						</li>
+						<li>
+							<a href="/contact-us/" title="تماس با ما">تماس با ما</a>
+						</li>
+						<li>
+							<a href="https://shahrpanel.com/about-us/" title="چرا شهر پانل">چرا شهر پانل</a>
+						</li>
+						<li>
+							<a href="/blog/" title="بلاگ شهر پانل">بلاگ شهر پانل</a>
+						</li>
+					</ul>
+				</div>
+				<div class="c-footer__col">
+					<div class="c-footer__title">
+						لینک های مرتبط
+					</div>
+					<ul class="u-flex gap-sm u-flex--column">
+						<li>
+							<a href="/" title="ساندویچ پانل">ساندویچ پانل</a>
+						</li>
+						<li>
+							<a href="/#c-price-sec" title="قیمت ساندویچ پانل">قیمت ساندویچ پانل</a>
+						</li>
+						<li>
+							<a href="/order/" title="ثبت سفارش آنلاین">ثبت سفارش آنلاین</a>
+						</li>
+						<li>
+							<a href="/calculator/" title="محاسبه متراژ ساندویچ پانل">محاسبه متراژ ساندویچ پانل
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="c-footer__col">
+					<ul class="p-0 u-flex wrap gap-md m-0 space-between">
+						<li class="u-flex items-end justify-center">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/enamad.png"
+								 alt="برقآپ">
+						</li>
+						<li class="u-flex items-end justify-center">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer-license.png"
+								 alt="برقآپ">
+						</li>
+						<li class="u-flex items-end justify-center">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/samandehi.png"
+								 alt="برقآپ">
+						</li>
+						<li class="u-flex items-end justify-center">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/enamad.png"
+								 alt="برقآپ">
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="c-footer__copyright u-flex space-between">
+				<span>
+					کلیه حقوق وب‌سایت محفوظ و متعلق به شرکت برقآپ می‌باشد.
+				</span>
+			<div class="c-footer__social u-flex gap-md">
+				<a href="#">
+					<svg width="24" height="24" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M16.2748 43H28.5107C38.7072 43 42.7858 38.9214 42.7858 28.7249V16.489C42.7858 6.29248 38.7072 2.21387 28.5107 2.21387H16.2748C6.07828 2.21387 1.99966 6.29248 1.99966 16.489V28.7249C1.99966 38.9214 6.07828 43 16.2748 43Z"
+							stroke="#031232" stroke-opacity="0.7" stroke-width="3.05896" stroke-linecap="round"
+							stroke-linejoin="round"/>
+						<path
+							d="M22.3919 29.7449C26.3339 29.7449 29.5295 26.5493 29.5295 22.6073C29.5295 18.6653 26.3339 15.4697 22.3919 15.4697C18.4499 15.4697 15.2543 18.6653 15.2543 22.6073C15.2543 26.5493 18.4499 29.7449 22.3919 29.7449Z"
+							stroke="#031232" stroke-opacity="0.7" stroke-width="3.05896" stroke-linecap="round"
+							stroke-linejoin="round"/>
+						<path d="M33.8873 12.4111H33.911" stroke="#031232" stroke-opacity="0.7" stroke-width="4.07862"
+							  stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</a>
+				<a href="#">
+					<svg width="24" height="24" viewBox="0 0 44 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M11.4826 39.9351C14.5419 41.7707 18.213 42.7905 21.8842 42.7905C33.1015 42.7905 42.2794 33.6126 42.2794 22.3952C42.2794 11.1779 33.1015 2 21.8842 2C10.6668 2 1.48892 11.1779 1.48892 22.3952C1.48892 26.0664 2.50868 29.5336 4.1403 32.5929L2.38721 39.3355C1.99007 40.8629 3.40387 42.2433 4.92142 41.8097L11.4826 39.9351Z"
+							stroke="#031232" stroke-opacity="0.7" stroke-width="2.5" stroke-miterlimit="10"
+							stroke-linecap="round" stroke-linejoin="round"/>
+						<path
+							d="M31.0617 28.2042C31.0617 28.5346 30.988 28.8742 30.8318 29.2046C30.6756 29.535 30.4735 29.8471 30.2069 30.1408C29.7564 30.6364 29.2602 30.9943 28.6995 31.2237C28.148 31.4532 27.5504 31.5725 26.907 31.5725C25.9696 31.5725 24.9676 31.3522 23.9105 30.9025C22.8536 30.4528 21.7965 29.8471 20.7486 29.0853C19.6915 28.3144 18.6897 27.4608 17.7338 26.5155C16.7871 25.561 15.9322 24.5606 15.1693 23.5143C14.4156 22.4681 13.809 21.4218 13.3678 20.3847C12.9266 19.3384 12.706 18.338 12.706 17.3835C12.706 16.7594 12.8163 16.1629 13.0369 15.6122C13.2575 15.0524 13.6067 14.5384 14.0939 14.0795C14.6822 13.5013 15.3256 13.2168 16.0058 13.2168C16.2631 13.2168 16.5205 13.2719 16.7503 13.382C16.9893 13.4921 17.2007 13.6573 17.3661 13.896L19.4986 16.8971C19.664 17.1266 19.7835 17.3377 19.8663 17.5396C19.9489 17.7323 19.995 17.925 19.995 18.0994C19.995 18.3197 19.9306 18.54 19.8019 18.751C19.6824 18.9621 19.5078 19.1824 19.2871 19.4027L18.5886 20.1277C18.4876 20.2287 18.4415 20.348 18.4415 20.4948C18.4415 20.5683 18.4507 20.6325 18.469 20.7059C18.4968 20.7793 18.5243 20.8344 18.5427 20.8895C18.7081 21.1924 18.993 21.587 19.3974 22.0642C19.8111 22.5415 20.2522 23.0279 20.7303 23.5143C21.2267 24.0008 21.7045 24.4505 22.1918 24.8635C22.6696 25.2673 23.0649 25.5427 23.3774 25.7079C23.4235 25.7262 23.4785 25.7537 23.543 25.7813C23.6164 25.8088 23.69 25.818 23.7726 25.818C23.9291 25.818 24.0484 25.7629 24.1495 25.662L24.8481 24.9736C25.0779 24.7442 25.2986 24.5698 25.5099 24.4597C25.7214 24.3312 25.9327 24.2669 26.1625 24.2669C26.3371 24.2669 26.5211 24.3036 26.7232 24.3862C26.9255 24.4688 27.1368 24.5882 27.3667 24.7442L30.409 26.901C30.6481 27.0662 30.8135 27.2589 30.9146 27.4884C31.0066 27.7178 31.0617 27.9473 31.0617 28.2042Z"
+							stroke="#031232" stroke-opacity="0.7" stroke-width="2.5" stroke-miterlimit="10"/>
+					</svg>
+				</a>
+				<a href="#">
+					<svg width="24" height="24" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M32.7849 42.7905H12.3897C6.27109 42.7905 2.19205 37.6917 2.19205 30.0434V14.747C2.19205 7.09881 6.27109 2 12.3897 2H32.7849C38.9035 2 42.9825 7.09881 42.9825 14.747V30.0434C42.9825 37.6917 38.9035 42.7905 32.7849 42.7905Z"
+							stroke="#031232" stroke-opacity="0.7" stroke-width="2.5" stroke-miterlimit="10"
+							stroke-linecap="round" stroke-linejoin="round"/>
+						<path
+							d="M21.3643 16.0206L26.4631 19.8447C28.2986 21.3743 28.2986 23.6688 26.4631 25.1984L21.3643 29.0225C19.3247 30.5522 17.6931 29.2775 17.6931 26.4731V18.8249C17.6931 15.5107 19.3247 14.4909 21.3643 16.0206Z"
+							stroke="#031232" stroke-opacity="0.7" stroke-width="2.5" stroke-miterlimit="10"
+							stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	</div>
+	<a class="catalog absolute u-flex items-center gap-sm" href="#">
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M12 8V2L10 4" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+			<path d="M12 2L14 4" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+			<path d="M7 12C3 12 3 13.79 3 16V17C3 19.76 3 22 8 22H16C20 22 21 19.76 21 17V16C21 13.79 21 12 17 12C16 12 15.72 12.21 15.2 12.6L14.18 13.68C13 14.94 11 14.94 9.81 13.68L8.8 12.6C8.28 12.21 8 12 7 12Z" stroke="#fff" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+			<path d="M5 12V10C5 7.99004 5 6.33004 8 6.04004" stroke="#fff" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+			<path d="M19 12V10C19 7.99004 19 6.33004 16 6.04004" stroke="#fff" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+
+
+		دانلود کاتالوگ
+	</a>
+</footer>
 <?php wp_footer(); ?>
 
 </body>
