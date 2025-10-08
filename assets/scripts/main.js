e45b21dc8b45ebd69d58bb0561f51a7bae468fb0
Hengameh Valizade <hengamehvalizade@gmail.com>
2025-10-08 10:50:27 +0330

    added: tab to header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,11 +10,9 @@
       var faqs = document.querySelectorAll(".faq-item");
       faqs.forEach(function (faq) {
         faq.querySelector(".faq-question").addEventListener("click", function () {
-          // بستن بقیه آیتم‌ها
           faqs.forEach(function (item) {
             if (item !== faq) item.classList.remove("active");
           });
-          // باز یا بسته کردن آیتم فعلی
           faq.classList.toggle("active");
         });
       });
@@ -22,7 +20,7 @@
     window.addEventListener("scroll", function () {
       var header = document.querySelector("#masthead");
       var scrollPosition = window.scrollY;
-      if (scrollPosition > 30) {
+      if (scrollPosition > 48) {
         header.classList.add("fixed-header");
       } else {
         header.classList.remove("fixed-header");
--- src/scripts/custom/main.js
+++ src/scripts/custom/main.js
@@ -7,11 +7,9 @@
 		  const faqs = document.querySelectorAll(".faq-item");
 		  faqs.forEach(faq => {
 			  faq.querySelector(".faq-question").addEventListener("click", () => {
-				  // بستن بقیه آیتم‌ها
 				  faqs.forEach(item => {
 					  if (item !== faq) item.classList.remove("active");
 				  });
-				  // باز یا بسته کردن آیتم فعلی
 				  faq.classList.toggle("active");
 			  });
 		  });
@@ -20,7 +18,7 @@
 		  const header = document.querySelector("#masthead");
 		  const scrollPosition = window.scrollY;
 
-		  if (scrollPosition > 30) {
+		  if (scrollPosition > 48) {
 			  header.classList.add("fixed-header");
 		  } else {
 			  header.classList.remove("fixed-header");
